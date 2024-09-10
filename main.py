b92e82faba87a824be4f006976b49cb6e6ff06c1
gsam1 <gsamardjiev@gmail.com>
2024-09-10 10:33:51 +0300

    Implemented caching;

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import streamlit as st
 import requests
 
 from loguru import logger
+from utils import DataframeCache
 
 
 load_dotenv()
@@ -25,73 +26,86 @@ def get_nyse_stocks():
     nyse_stocks = tables[0]
 
     # Extract the tickers
-    tickers = nyse_stocks['Symbol'].tolist()
+    tickers = nyse_stocks["Symbol"].tolist()
 
     return nyse_stocks, tickers
 
 
 def check_fields_if_valid(fields: list[str]) -> bool:
     expected_fields = [
-        'Current Assets',
-        'Current Liabilities',
-        'Inventory',
-        'Cash Cash Equivalents And Short Term Investments',
-        'Total Liabilities Net Minority Interest',
-        'Common Stock Equity',
-        'Current Capital Lease Obligation'
+        "Current Assets",
+        "Current Liabilities",
+        "Inventory",
+        "Cash Cash Equivalents And Short Term Investments",
+        "Total Liabilities Net Minority Interest",
+        "Common Stock Equity",
+        "Current Capital Lease Obligation",
     ]
 
     result = [f in fields for f in expected_fields]
 
     return sum(result) == len(expected_fields)
 
+
 # Function to fetch financial data
 def get_financial_data(ticker):
     stock = yf.Ticker(ticker)
-    
+
     try:
         # Get balance sheet data
         balance_sheet = stock.quarterly_balancesheet
         nil_pos_col = balance_sheet.columns[0]
         info = stock.info
-        
+
         if check_fields_if_valid(list(balance_sheet.index)):
-            logger.info(f'Adding Ticker {ticker}')
-            current_assets = balance_sheet.loc['Current Assets', nil_pos_col]
-            current_liabilities = balance_sheet.loc['Current Liabilities', nil_pos_col]
-            inventory = balance_sheet.loc['Inventory', nil_pos_col]
-            cash_equivalents = balance_sheet.loc['Cash Cash Equivalents And Short Term Investments', nil_pos_col]
-            total_liabilities = balance_sheet.loc['Total Liabilities Net Minority Interest', nil_pos_col]
-            common_equity = balance_sheet.loc['Common Stock Equity', nil_pos_col]
+            logger.info(f"Adding Ticker {ticker}")
+            current_assets = balance_sheet.loc["Current Assets", nil_pos_col]
+            current_liabilities = balance_sheet.loc["Current Liabilities", nil_pos_col]
+            inventory = balance_sheet.loc["Inventory", nil_pos_col]
+            cash_equivalents = balance_sheet.loc[
+                "Cash Cash Equivalents And Short Term Investments", nil_pos_col
+            ]
+            total_liabilities = balance_sheet.loc[
+                "Total Liabilities Net Minority Interest", nil_pos_col
+            ]
+            common_equity = balance_sheet.loc["Common Stock Equity", nil_pos_col]
 
             # Calculations
             current_ratio = current_assets / current_liabilities
-            quick_ratio = (current_assets - inventory) / current_liabilities  # If inventory is not available, use cash equivalents
+            quick_ratio = (
+                current_assets - inventory
+            ) / current_liabilities  # If inventory is not available, use cash equivalents
             debt_to_equity = total_liabilities / common_equity
-            capital_lease_obligation = balance_sheet.loc['Current Capital Lease Obligation', nil_pos_col]
-            free_cash_flow = cash_equivalents - capital_lease_obligation if capital_lease_obligation is not None else np.nan # Rough substitute
-            
+            capital_lease_obligation = balance_sheet.loc[
+                "Current Capital Lease Obligation", nil_pos_col
+            ]
+            free_cash_flow = (
+                cash_equivalents - capital_lease_obligation
+                if capital_lease_obligation is not None
+                else np.nan
+            )  # Rough substitute
+
             # Dividend Yield and Payout Ratio
-            pe_ratio = info.get('forwardPE', np.nan)
-            dividend_yield = info.get('dividendYield', np.nan)
-            payout_ratio = info.get('payoutRatio', np.nan)
+            pe_ratio = info.get("forwardPE", np.nan)
+            dividend_yield = info.get("dividendYield", np.nan)
+            payout_ratio = info.get("payoutRatio", np.nan)
 
             # Store all in a dictionary
             data = {
-                'Ticker': ticker,
-                'Current Ratio': current_ratio,
-                'Quick Ratio': quick_ratio,
-                'Debt to Equity': debt_to_equity,
-                'P/E Ratio': pe_ratio,
-                'Dividend Yield': dividend_yield,
-                'Payout Ratio': payout_ratio,
-                'Free Cash Flow': free_cash_flow,
-            }    
+                "Ticker": ticker,
+                "Current Ratio": current_ratio,
+                "Quick Ratio": quick_ratio,
+                "Debt to Equity": debt_to_equity,
+                "P/E Ratio": pe_ratio,
+                "Dividend Yield": dividend_yield,
+                "Payout Ratio": payout_ratio,
+                "Free Cash Flow": free_cash_flow,
+            }
             return data
         else:
             return None
     except Exception as e:
-        logger.info(f'Ticker failed: {ticker} with {e}')
+        logger.info(f"Ticker failed: {ticker} with {e}")
         return None
 
 
@@ -102,8 +116,7 @@ def get_fmp_data(ticker: str, api_key: str) -> dict:
 
         return resp.json()
     except Exception as e:
-        print(e)    
-
+        print(e)
 
 
 # Fetch data for all tickers
@@ -111,48 +124,70 @@ def filter_data_to_criteria(stock_data: pd.DataFrame, criteria: dict) -> pd.Data
     df = pd.DataFrame(stock_data)
     # Apply the screening criteria
     filtered_stocks = df[
-        (df['Current Ratio'] > criteria['current_ratio']) &
-        (df['Debt to Equity'] < criteria['debt_to_equity']) &
-        (df['P/E Ratio'] < criteria['pe_ratio']) &
-        (df['Dividend Yield'] > criteria['dividend_yield']) &
-        (df['Payout Ratio'] < criteria['payout_ratio']) &
-        (df['Free Cash Flow'] > 0)  # Ensure positive free cash flow
+        (df["Current Ratio"] > criteria["current_ratio"])
+        & (df["Debt to Equity"] < criteria["debt_to_equity"])
+        & (df["P/E Ratio"] < criteria["pe_ratio"])
+        & (df["Dividend Yield"] > criteria["dividend_yield"])
+        & (df["Payout Ratio"] < criteria["payout_ratio"])
+        & (df["Free Cash Flow"] > 0)  # Ensure positive free cash flow
     ]
 
     return filtered_stocks
 
+
 def main() -> None:
     # Streamlit App
     st.title("Stock Screener App")
-
+    cache = DataframeCache()
     # Adjust financial ratios using Streamlit sliders
     st.sidebar.header("Set Financial Ratios")
     current_ratio_min = st.sidebar.slider("Minimum Current Ratio", 0.0, 10.0, 1.5)
-    debt_to_equity_max = st.sidebar.slider("Maximum Debt to Equity Ratio", 0.0, 5.0, 0.5)
+    debt_to_equity_max = st.sidebar.slider(
+        "Maximum Debt to Equity Ratio", 0.0, 5.0, 0.5
+    )
     pe_ratio_max = st.sidebar.slider("Maximum P/E Ratio", 0, 100, 20)
     dividend_yield_min = st.sidebar.slider("Minimum Dividend Yield (%)", 0.0, 10.0, 2.0)
     payout_ratio_max = st.sidebar.slider("Maximum Payout Ratio", 0.0, 1.0, 0.6)
 
     if st.button("Run Stock Screener"):
         criteria = {
-            'current_ratio': current_ratio_min,
-            'debt_to_equity': debt_to_equity_max,
-            'dividend_yield': dividend_yield_min / 100,  # 2%
-            'payout_ratio': payout_ratio_max,
-            'pe_ratio': pe_ratio_max,
+            "current_ratio": current_ratio_min,
+            "debt_to_equity": debt_to_equity_max,
+            "dividend_yield": dividend_yield_min / 100,  # 2%
+            "payout_ratio": payout_ratio_max,
+            "pe_ratio": pe_ratio_max,
         }
 
         stocks, symbols = get_nyse_stocks()
-        stocks['Ticker'] = stocks['Symbol']
-        stock_fin_list = [get_financial_data(ticker) for ticker in stqdm(symbols)]
-        stock_fin_list = [item for item in stock_fin_list if item]
-        stock_data = pd.DataFrame(stock_fin_list)
+        stocks["Ticker"] = stocks["Symbol"]
+        if cache.is_stale():
+            stock_fin_list = [get_financial_data(ticker) for ticker in stqdm(symbols)]
+            stock_fin_list = [item for item in stock_fin_list if item]
+            stock_data = pd.DataFrame(stock_fin_list)
+            cache.write_cache(stock_data)
+        else:
+            stock_data = cache.load_cache()
+
         filtered_df = filter_data_to_criteria(stock_data=stock_data, criteria=criteria)
 
-        merge_df = pd.merge(filtered_df, stocks, on='Ticker')
+        merge_df = pd.merge(filtered_df, stocks, on="Ticker")
         st.write("Filtered Stocks:")
-        st.dataframe(merge_df.loc[:, ['Ticker', 'GICS Sector', 'GICS Sub-Industry', 'Current Ratio', 'Debt to Equity', 'P/E Ratio', 'Dividend Yield', 'Payout Ratio']])
+        st.dataframe(
+            merge_df.loc[
+                :,
+                [
+                    "Ticker",
+                    "GICS Sector",
+                    "GICS Sub-Industry",
+                    "Current Ratio",
+                    "Debt to Equity",
+                    "P/E Ratio",
+                    "Dividend Yield",
+                    "Payout Ratio",
+                ],
+            ]
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -4,14 +4,52 @@ import pandas as pd
 
 
 class DataframeCache:
-    def __init__(self, cache_file_location: str = "/tmp/") -> None:
-        pass
+    cache_dt_format: str = "%Y_%m_%d_%H_%M"
+
+    def __init__(
+        self, cache_file_location: str = "/tmp/", cache_stale_days: int = 60
+    ) -> None:
+        self.cache_file_location = cache_file_location
+        self.cache_stale_days = cache_stale_days
 
     def write_cache(self, df: pd.DataFrame) -> None:
         dt = datetime.now()
-        formatted_string = dt.strftime("%Y_%m_%d_%H_%M")
-        filename = f'fin_screen_{formatted_string}.csv'
-        pass
+        formatted_string = dt.strftime(self.cache_dt_format)
+        filename = f"{formatted_string}_fin_screen.csv"
+        file_full_path = os.path.join(self.cache_file_location, filename)
+        df.to_csv(file_full_path, index=False)
 
-    def load_cache(self) -> None:
-        pass
+    def _check_file_is_cache(self, filename: str) -> bool:
+        if filename.endswith(".csv") and "fin_screen" in filename:
+            return True
+        else:
+            return False
+
+    def _get_cache_files(self) -> list[str]:
+        cache_files = sorted(
+            [
+                os.path.join(self.cache_file_location, f)
+                for f in os.listdir(self.cache_file_location)
+                if self._check_file_is_cache(f)
+            ],
+        )
+        return cache_files
+
+    def load_cache(self) -> pd.DataFrame:
+        cache_files = self._get_cache_files()
+        return pd.read_csv(cache_files[-1])
+
+    def is_stale(self) -> bool:
+        cache_files = self._get_cache_files()
+        if len(cache_files) == 0:
+            return True
+        else:
+            latest_cache = cache_files[-1]
+            latest_cache_date = datetime.strptime(
+                latest_cache.split("fin_screen")[0].split('/')[-1][:-1], self.cache_dt_format
+            )
+
+            if (datetime.now() - latest_cache_date).days > self.cache_stale_days:
+                return True
+            else:
+                return False
